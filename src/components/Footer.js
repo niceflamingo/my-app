6bb4efc40999fac5c9c5072a085677e077aef90c
niceFlamingo <104111513+niceflamingo@users.noreply.github.com>
2022-07-05 15:44:08 -0700

    Added Components and Assets

---
+++ src/components/Footer.js
@@ -0,0 +1,33 @@
+import { Container, Row, Col } from "react-bootstrap";
+import logo from "../assets/img/logo.svg";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon3 from "../assets/img/nav-icon3.svg";
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          <Col size={12} sm={6}>
+            <img src={logo} alt="Logo" />
+          </Col>
+          <Col size={12} sm={6} className="text-center text-sm-end">
+            <div className="social-icon">
+              <a href="https://www.linkedin.com/in/brian-balderrama/">
+                <img src={navIcon1} alt="LinkedIn Icon" />
+              </a>
+              <a href="https://github.com/niceflamingo">
+                <img src={navIcon2} alt="GitHub Icon" />
+              </a>
+              <a href="#">
+                <img src={navIcon3} alt="Instagram Icon" />
+              </a>
+            </div>
+            <p>Copyright 2022. All Rights Reserved. Lou Dog Inc. </p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
---
+++ src/components/NavBar.js
@@ -0,0 +1,103 @@
+import { useState, useEffect } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import logo from "../assets/img/logo4.svg";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon3 from "../assets/img/nav-icon3.svg";
+
+export const NavBar = () => {
+  const [activeLink, setActiveLink] = useState("home");
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  };
+
+  return (
+    <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+      <Container>
+        <Navbar.Brand href="/">
+          <img src={logo} alt="Logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span className="navbar-toggler-icon"></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link
+              href="#home"
+              className={
+                activeLink === "home" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("home")}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              href="#skills"
+              className={
+                activeLink === "skills" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("skills")}
+            >
+              Skills
+            </Nav.Link>
+            <Nav.Link
+              href="#project"
+              className={
+                activeLink === "projects" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("Projects")}
+            >
+              Projects
+            </Nav.Link>
+
+            <Nav.Link
+              href="../assets/img/Brian-Balderrama-Resume.pdf"
+              download
+              className={
+                activeLink === "resume" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("Projects")}
+            >
+              Download Resume
+            </Nav.Link>
+          </Nav>
+          <span className="navbar-text">
+            <div className="social-icon">
+              <a href="https://www.linkedin.com/in/brian-balderrama/">
+                <img src={navIcon1} alt="LinkedIn" />
+              </a>
+              <a href="https://github.com/niceflamingo">
+                <img src={navIcon2} alt="GitHub" />
+              </a>
+              <a href="#">
+                <img src={navIcon3} alt="Instagram" />
+              </a>
+            </div>
+            <button
+              className="vvd"
+              onClick={() => window.location.replace("/#connect")}
+            >
+              <span>Let's Connect</span>
+            </button>
+          </span>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
